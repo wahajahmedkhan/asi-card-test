47b31b439a9ba182fbaac4abb17bede9f09ea2c1
Wahaj Ahmed Khan <wahajahmedkhan95@gmail.com>
2022-05-19 00:49:54 +0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'card-test';
+  cards = [
+    { icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png', title: 'Title 1', subTitle: 'Subtitle 1', description: 'Description 1. some long text goes here. Please enable JavaScript to continue using this application.', border_color:'#333' },
+    { icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png', title: 'Title 2', subTitle: 'Subtitle 2', description: 'Description 2', border_color:'#e71010'  },
+    { icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png', title: 'Title 2', subTitle: 'Subtitle 2', description: 'Description 2', border_color:'#f1e40f'  },
+    { icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png', title: 'Title 2', subTitle: 'Subtitle 2', description: 'Description 2', border_color:'#0ebe2b' },
+    { icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png', title: 'Title 2', subTitle: 'Subtitle 2', description: 'Description 2', border_color:'#1224e3'  },
+  ];
+
 }
